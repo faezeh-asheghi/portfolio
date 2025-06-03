45944d5352ccc4e2a3d4ff05e054954367235a3f
Faeze Asheghi <f.asheghi@tes.ir>
2025-06-03 12:26:41 +0330

    edit

--- app/component/About.tsx
+++ app/component/About.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import { motion } from "framer-motion";
 import Image from "next/image";
 import groovyWalkAnimation from "./Animation.json";
 import {useLottie} from "lottie-react";
@@ -16,19 +16,33 @@ export default function About() {
 
     return (
 
-<div className="flex lg:flex-row md:flex-row flex-col gap-10 absolute top-0 right-0 z-0 bg-red w-full">
 
 <div>
 <Image className={"absolute top-0 right-0 z-0"} width={600} height={400}  src={"/about.png"} alt={"logo"} />
-    <div className={'absolute  top-10  z-1 lg:scale-85 md:scale-75 scale-55 '}>
+    <div className={'absolute lg:top-10 md:top-10  top-5 lg:right-0 md:right-0 right-0 z-1 lg:scale-120 md:scale-75 scale-85 '}>
         {View}
-    </div></div>
-    <div className="flex flex-col  absolute left-0 px-[120px] py-[110px]">
-        <p className="text-xs font-extralight text-[#FDC435]">Frontend Developer</p>
-
     </div>
-
+<div className={"lg:absolute lg:top-10 lg:left-2 flex flex-col gap-4 w-full justify-end align-middle items-center"}>
+    <motion.p
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.6, duration: 1 }}
+        className="text-xs  md:text-2xl text-[#FDC435]"
+    >
+       Front End Development
+    </motion.p>
+    <motion.h1
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        className="lg:text-4xl text-xl md:text-4xl  font-bold mb-4"
+    >
+        Hello,  my name is Faeze Asheghi
+    </motion.h1></div>
 </div>
+
+
+
 
     );
 }
