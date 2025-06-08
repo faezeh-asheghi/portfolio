566aca984d573c89edd4b99ca6d8ebc5711e44a6
faezeh asheghi <f.asheghi@tes.ir>
2025-06-08 12:01:47 -0700

    add skills

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import About from "@/app/component/About";
-import IntroOverlay from "@/app/component/IntroOverlay";
 import Skills from "@/app/component/Skills";
 
 export default function Home() {
   return (
     <>
 
-      <div className="bg-[#F9FAFF] py-0 flex flex-col md:gap-50 gap-30 lg:gap-50 items-center justify-end px-8 sm:px-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="bg-[#F9FAFF] overflow-x-hidden py-0 flex flex-col md:gap-50 gap-30 lg:gap-50 items-center justify-end px-8 sm:px-20 font-[family-name:var(--font-geist-sans)]">
         <div className="w-full h-[300px]"></div>
+        
         <About />
         <Skills/>
 
